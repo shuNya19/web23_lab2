82b8b1ff88ee9e06dd8753439d448162f297fb4d
kardackow.max@gmail.com <kardackow.max@gmail.com>
2023-11-20 13:25:41 +0300

    task completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,8 @@
 const gulp = require('gulp');
-const opn = require('opn');
 
-const pages = [
-  'https://github.com',
-  'https://habr.com',
-  'https://stackoverflow.com',
-];
-
-const interval = 5000;
-let currentPageIndex = 0;
-let intervalId; 
-
-function showNextPage() {
-  if (currentPageIndex < pages.length) {
-    opn(pages[currentPageIndex]).catch(error => {
-      console.error(`Error opening page: ${error}`);
-    });
-    currentPageIndex++;
-  } else {
-    clearInterval(intervalId);
-    console.log('All pages have been opened. Task stopped.');
-  }
+function copyHTML() {
+  return gulp.src('dir1/*.html')
+    .pipe(gulp.dest('dir2'));
 }
 
-gulp.task('show-pages', function (done) {
-  intervalId = setInterval(showNextPage, interval);
-  done();
-});
-
-gulp.task('default', gulp.series('show-pages'));
+gulp.task('copy_paste', copyHTML); 
